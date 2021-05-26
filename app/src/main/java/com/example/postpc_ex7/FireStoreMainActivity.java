b4dde7e8952a7a6a3fd3c7784692942db0abc0a7
gadi <gadi.fleischer@mail.huji.ac.il>
2021-05-26 12:35:50 +0300

    init project

---
+++ app/src/main/java/com/example/postpc_ex7/FireStoreMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.postpc_ex7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FireStoreMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firestore_activity_main);
+    }
+}
